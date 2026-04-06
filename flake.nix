861e9f046254cd51640852e83aefbbf9fed939e9
Yifei Sun <ysun@hey.com>
2026-04-06 18:57:13 +0200

    expose overlay

--- flake.nix
+++ flake.nix
@@ -2,17 +2,17 @@
   outputs = { self, nixpkgs, parts, systems } @ inputs: parts.lib.mkFlake { inherit inputs; } {
     systems = import systems;
 
+    flake.overlays.default = (final: prev: {
+      ocamlPackages = prev.ocaml-ng.ocamlPackages.overrideScope (ocamlFinal: ocamlPrev:
+        (with nixpkgs.lib; genAttrs
+          (attrNames (builtins.readDir ./pkgs))
+          (name: ocamlFinal.callPackage ./pkgs/${name} { })));
+    });
+
     perSystem = { lib, pkgs, system, self', ... }: {
       _module.args.pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          (final: prev: {
-            ocamlPackages = prev.ocaml-ng.ocamlPackages.overrideScope (ocamlFinal: ocamlPrev:
-              (with lib; genAttrs
-                (attrNames (builtins.readDir ./pkgs))
-                (name: ocamlFinal.callPackage ./pkgs/${name} { })));
-          })
-        ];
+        overlays = [ self.overlays.default ];
       };
 
       devShells.default = pkgs.mkShell {
@@ -55,10 +55,8 @@
     };
   };
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    parts.url = "github:hercules-ci/flake-parts";
-    parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    systems.url = "github:nix-systems/default";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  inputs.systems.url = "github:nix-systems/default";
 }
